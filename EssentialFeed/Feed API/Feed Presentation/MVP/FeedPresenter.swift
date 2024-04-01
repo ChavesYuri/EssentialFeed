d59724b3e2f7666c798c6ece76d3b931448e7160
ChavesYuri <33982282+ChavesYuri@users.noreply.github.com>
2024-04-01 10:58:14 -0300

    new FeedPresenter as a mapper

--- EssentialFeed/Feed API/Feed Presentation/MVP/FeedPresenter.swift
+++ EssentialFeed/Feed API/Feed Presentation/MVP/FeedPresenter.swift
@@ -12,6 +12,19 @@ public protocol FeedErrorView {
     func display(_ viewModel: FeedErrorViewModel)
 }
 
+public final class FeedPresenter {
+    public static var title: String {
+        NSLocalizedString("FEED_VIEW_TITLE",
+                          tableName: "Feed",
+                          bundle: Bundle(for: FeedPresenter.self),
+                          comment: "Title for the feed view")
+    }
+    
+    public static func map(_ feed: [FeedImage]) -> FeedViewModel {
+        FeedViewModel(feed: feed)
+    }
+}
+
 public final class OldFeedPresenter {
     private let feedView: FeedView
     private let loadingView: FeedLoadingView
